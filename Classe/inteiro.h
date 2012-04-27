16fad39c331cf2ef22fe5d5907c3906caa56f5df
Maicon Vieira <maiconv@gmail.com>
2012-04-26 22:42:41 -0300

    Classes

---
+++ Classe/inteiro.h
@@ -0,0 +1,7 @@
+class inteiro{
+public:
+
+    int valor;
+
+};
+
---
+++ Classe/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "inteiro.h"
+
+using namespace std;
+
+int main()
+{
+    inteiro i ;
+    inteiro x ;
+
+    cout <<"Digite um numero:";
+    cin >> i.valor;
+
+    cout <<"Digite um numero:";
+    cin >> x.valor;
+
+    cout << "Primeiro: " << i.valor << "\n";
+    cout << "Segundo: "<< x.valor << "\n";
+
+}
+
+//classe ponto;
+//
